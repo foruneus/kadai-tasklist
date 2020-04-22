ee5c0f826cc9f4c6df6426699d57f93f398749e8
foruneus <tear.tm.r@gmail.com>
2020-04-22 06:34:57 +0000

    CRUD tasklist

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'TasksController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('task', 'TasksController');
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('Task/{id}', 'TasksController@show');
+// Route::post('Task', 'TasksController@store');
+// Route::put('Task/{id}', 'TasksController@update');
+// Route::delete('Task/{id}', 'TasksController@destroy');
+
+// index: showの補助ページ
+// Route::get('Task', 'TasksController@index')->name('task.index');
+// create: 新規作成用のフォームページ
+// Route::get('Task/create', 'TasksController@create')->name('Task.create');
+// edit: 更新用のフォームページ
+// Route::get('Task/{id}/edit', 'TasksController@edit')->name('Task.edit');
+
+
