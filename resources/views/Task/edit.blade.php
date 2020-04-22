16177ebec9b0f8a668f36d4d7d689f8ceb69390e
foruneus <tear.tm.r@gmail.com>
2020-04-22 06:37:32 +0000

    CRUD tasklist

--- resources/views/Task/edit.blade.php
+++ resources/views/Task/edit.blade.php
@@ -7,14 +7,14 @@
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => ['task.update', $task->id], 'method' => 'put']) !!}
-        
+
                 <div class="form-group">
                     {!! Form::label('content', '内容:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
-        
+
                 {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
-        
+
             {!! Form::close() !!}
         </div>
     </div>
